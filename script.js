007af13cfe3bfcf5cf086872d16048933015f33f
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2024-02-04 01:34:49 -0800

    method

---
+++ script.js
@@ -0,0 +1,14 @@
+// Array
+
+let myClass = ["Mahnaz", "Mina", " Ali", "Zahra", "Mamad"];
+
+console.log(myClass);
+// myClass[5] = "Alireza";
+// console.log(myClass);
+// console.log(myClass[0]);
+
+
+
+// Array method
+
+// console.log(myClass.join())
